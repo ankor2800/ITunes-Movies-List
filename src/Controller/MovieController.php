68e2b340ea87b2246e7c2557bca949dab71884e3
Artem Korneychuk <ankor2800@gmail.com>
2021-02-07 03:17:44 +0300

    added new controller

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use function Amp\Promise\rethrow;
+use App\Container\ServiceNotFoundException;
+use App\Entity\Movie;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Interfaces\RouteCollectorInterface;
+use Twig\Environment;
+
+class MovieController
+{
+    public function __construct(
+        private RouteCollectorInterface $routeCollector,
+        private Environment $twig,
+        private EntityManagerInterface $em
+    ) {}
+
+    public function index(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        try {
+            $data = $this->twig->render('movie.html.twig', [
+                'movie'    => $this->getData((int) $request->getAttribute('movie_id')),
+                'homepage' => $this->routeCollector->getRouteParser()->urlFor('main'),
+            ]);
+        } catch (\Exception $e) {
+            throw new HttpBadRequestException($request, $e->getMessage(), $e);
+        }
+
+        $response->getBody()->write($data);
+
+        return $response;
+    }
+
+    protected function getData(int $id): Movie
+    {
+        return $this->em->getRepository(Movie::class)
+            ->find($id);
+    }
+}
--- src/Provider/WebProvider.php
+++ src/Provider/WebProvider.php
@@ -4,12 +4,14 @@ namespace App\Provider;
 
 use App\Container\Container;
 use App\Controller\HomeController;
+use App\Controller\MovieController;
 use App\Support\Config;
 use App\Support\ServiceProviderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Slim\Interfaces\RouteCollectorInterface;
 use Slim\Interfaces\RouteCollectorProxyInterface;
+use Slim\Interfaces\RouteInterface;
 use Symfony\Component\Yaml\Yaml;
 use Twig\Environment;
 
@@ -25,6 +27,10 @@ class WebProvider implements ServiceProviderInterface
     {
         $container->set(HomeController::class, static function (ContainerInterface $container) {
             return new HomeController($container->get(RouteCollectorInterface::class), $container->get(Environment::class), $container->get(EntityManagerInterface::class));
+        });
+
+        $container->set(MovieController::class, static function (ContainerInterface $container) {
+            return new MovieController($container->get(RouteCollectorInterface::class), $container->get(Environment::class), $container->get(EntityManagerInterface::class));
         });
     }
 
